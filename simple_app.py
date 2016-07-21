186b22bda6cf75f4692fa34f589adaa78260e1ba
Alan Chu <chu.chuyalan@gmail.com>
2016-07-21 12:48:34 -0400

    two different views add & multiply are working and accept different data types because of multiple decorators

--- simple_app.py
+++ simple_app.py
@@ -1,11 +1,33 @@
 from flask import Flask
-from flask import request
 
 app = Flask(__name__)
 
 @app.route('/')
+@app.route("/<name>")
 def index(name="Buddy"):
-    name = request.args.get("name", name)
     return "Hello {}!".format(name)
+
+# default data type it comes in is a string!
+@app.route('/add/<int:num1>/<int:num2>')
+@app.route('/add/<float:num1>/<float:num2>')
+@app.route('/add/<int:num1>/<float:num2>')
+@app.route('/add/<float:num1>/<int:num2>')
+def add(num1,num2):
+    return "{} + {} = {}".format(num1, num2, num1+num2)
+
+# @app.route('/multiply')
+# @app.route('/multiply/<int:num1>/<int:num2>')
+# @app.route('/multiply/<float:num1>/<float:num2>')
+# @app.route('/multiply/<int:num1>/<float:num2>')
+# @app.route('/multiply/<float:num1>/<int:num2>')
+# def multiply(num1=5, num2=5):
+#     return "{} *{} = {}".format(num1, num2, num1*num2)
+@app.route('/multiply')
+@app.route('/multiply/<int:num1>/<int:num2>')
+@app.route('/multiply/<float:num1>/<float:num2>')
+@app.route('/multiply/<int:num1>/<float:num2>')
+@app.route('/multiply/<float:num1>/<int:num2>')
+def multiply(num1=5, num2=5):
+    return str(num1*num2)
 
 app.run(debug=True)
