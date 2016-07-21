28e6d2419c1a734c41a53746a6d47a2b3fe6f7ff
Alan Chu <chu.chuyalan@gmail.com>
2016-07-21 13:28:06 -0400

    Templates :) and using template inheritance to be more DRY, super cool mahn

--- simple_app.py
+++ simple_app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
@@ -13,7 +14,9 @@ def index(name="Buddy"):
 @app.route('/add/<int:num1>/<float:num2>')
 @app.route('/add/<float:num1>/<int:num2>')
 def add(num1,num2):
-    return "{} + {} = {}".format(num1, num2, num1+num2)
+    context = {'num1': num1, "num2": num2}
+    return render_template("add.html", **context)
+    # return render_template("add.html", num1=num1, num2=num2)
 
 # @app.route('/multiply')
 # @app.route('/multiply/<int:num1>/<int:num2>')
@@ -28,6 +31,6 @@ def add(num1,num2):
 @app.route('/multiply/<int:num1>/<float:num2>')
 @app.route('/multiply/<float:num1>/<int:num2>')
 def multiply(num1=5, num2=5):
-    return str(num1*num2)
+    return render_template("multiply.html", num1=num1, num2=num2)
 
 app.run(debug=True)
