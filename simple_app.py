42da6f3830ec4e5b319a1aac5b92a50bc80e04cd
Alan Chu <chu.chuyalan@gmail.com>
2016-07-22 11:45:56 -0400

    working set cookie

--- simple_app.py
+++ simple_app.py
@@ -1,12 +1,14 @@
-from flask import Flask
-from flask import render_template
+from flask import (Flask, render_template, redirect,
+                    url_for, request, make_response)
+import json
 
 app = Flask(__name__)
 
 @app.route('/')
 @app.route("/<name>")
 def index(name="Buddy"):
-    return "Hello {}!".format(name)
+    return render_template("index.html", name = name)
+    # return "Hello {}!".format(name)
 
 # default data type it comes in is a string!
 @app.route('/add/<int:num1>/<int:num2>')
@@ -18,19 +20,22 @@ def add(num1,num2):
     return render_template("add.html", **context)
     # return render_template("add.html", num1=num1, num2=num2)
 
-# @app.route('/multiply')
-# @app.route('/multiply/<int:num1>/<int:num2>')
-# @app.route('/multiply/<float:num1>/<float:num2>')
-# @app.route('/multiply/<int:num1>/<float:num2>')
-# @app.route('/multiply/<float:num1>/<int:num2>')
-# def multiply(num1=5, num2=5):
-#     return "{} *{} = {}".format(num1, num2, num1*num2)
-@app.route('/multiply')
-@app.route('/multiply/<int:num1>/<int:num2>')
-@app.route('/multiply/<float:num1>/<float:num2>')
-@app.route('/multiply/<int:num1>/<float:num2>')
-@app.route('/multiply/<float:num1>/<int:num2>')
-def multiply(num1=5, num2=5):
-    return render_template("multiply.html", num1=num1, num2=num2)
+# create the route for saving & uploading cookie data
+@app.route('/save', methods=["POST"])
+def save():
+    # make the response
+    response = make_response(redirect(url_for('index')))
+    response.set_cookie("user", json.dumps(dict(request.form.items())))
+    # response.set_cookie("character", json.dumps(data))
+    # data = get_saved_data()
+    # data.update(dict(request.form.items()))
+    return response
+
+# def get_saved_data():
+#     try:
+#         data = json.loads(request.cookies.get('user'))
+#     except TypeError:
+#         data = {}
+#     return data
 
 app.run(debug=True)
